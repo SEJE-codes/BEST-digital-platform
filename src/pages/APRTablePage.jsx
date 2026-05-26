be160fc522c1103a420973141cd1ba37f1f4374c
SEJE-codes <modricalcantara610@gmail.com>
2026-05-26 13:40:39 +0200

    Updated

--- src/pages/APRTablePage.jsx
+++ src/pages/APRTablePage.jsx
@@ -21,132 +21,471 @@ function APRTablePage() {
   // BLOCS DATA
   // =====================================
 
-  const blocsData = {
+  const blocsData = { 
     BAT: [
-      {
-        code: "BAT01",
-        installation: "Bâtiment administratif",
-      },
-      {
-        code: "BAT02",
-        installation: "Parking",
-      },
-      {
-        code: "BAT03",
-        installation: "Zone BAT 03",
-      },
-      {
-        code: "BAT04",
-        installation: "Réfectoire",
-      },
-    ],
+    {
+      code: "BAT01",
+      installation: "Bâtiment administratif",
+    },
 
-    STE: [
-      {
-        code: "STE01",
-        installation: "Station de traitement des eaux",
-      },
-      {
-        code: "STE02",
-        installation: "Zone STE 02",
-      },
-    ],
+    {
+      code: "BAT02",
+      installation: "Parking",
+    },
 
-    MAG: [
-      {
-        code: "MAG01",
-        installation: "Tous les magasins du site",
-      },
-      {
-        code: "MAG06",
-        installation: "Zone de stockage des hydrocarbures",
-      },
-      {
-        code: "MAG11",
-        installation: "Cuve de gaz",
-      },
-    ],
+    {
+      code: "BAT03",
+      installation: "Zone BAT 03",
+    },
 
-    ZMT: [
-      {
-        code: "ZMT01",
-        installation: "Atelier maintenance",
-      },
-      {
-        code: "ZMT07",
-        installation: "Atelier et Garage",
-      },
-    ],
+    {
+      code: "BAT04",
+      installation: "Réfectoire",
+    },
+  ],
 
-    CON: [
-      {
-        code: "CON01",
-        installation: "Conditionnement",
-      },
-    ],
+  STE: [
+    {
+      code: "STE01",
+      installation: "Station de traitement des eaux",
+    },
 
-    MAC: [
-      {
-        code: "MAC01",
-        installation: "Chaudière",
-      },
-      {
-        code: "MAC04",
-        installation: "Compresseur des fluides (air CO2)",
-      },
-    ],
+    {
+      code: "STE02",
+      installation: "Zone STE 02",
+    },
 
-    TRA: [
-      {
-        code: "TRA01",
-        installation: "Matériels roulants",
-      },
-    ],
+    {
+      code: "STE03",
+      installation: "Zone STE 03",
+    },
 
-    ELT: [
-      {
-        code: "ELT01",
-        installation:
-          "Transformateur / Coffret électrique / Bloc électrique",
-      },
-      {
-        code: "ELT05",
-        installation: "Groupe électrogène",
-      },
-    ],
+    {
+      code: "STE04",
+      installation: "Zone STE 04",
+    },
+  ],
 
-    CMT: [
-      {
-        code: "CMT01",
-        installation: "Quai de dépotage",
-      },
-    ],
+  MAG: [
+    {
+      code: "MAG01",
+      installation: "Tous les magasins du site",
+    },
 
-    CAN: [
-      {
-        code: "CAN01",
-        installation: "Canalisation de fluide",
-      },
-    ],
+    {
+      code: "MAG02",
+      installation: "Magasin 02",
+    },
 
-    LAB: [
-      {
-        code: "LAB01",
-        installation: "Laboratoire",
-      },
-    ],
+    {
+      code: "MAG03",
+      installation: "Magasin 03",
+    },
 
-    SIL: [
-      {
-        code: "SIL01",
-        installation: "Zone d’ensilage",
-      },
-      {
-        code: "SIL19",
-        installation: "Station à gaz",
-      },
-    ],
-  };
+    {
+      code: "MAG04",
+      installation: "Magasin 04",
+    },
+
+    {
+      code: "MAG05",
+      installation: "Magasin 05",
+    },
+
+    {
+      code: "MAG06",
+      installation: "Zone de stockage des hydrocarbures",
+    },
+
+    {
+      code: "MAG07",
+      installation: "Magasin 07",
+    },
+
+    {
+      code: "MAG08",
+      installation: "Magasin 08",
+    },
+
+    {
+      code: "MAG09",
+      installation: "Magasin 09",
+    },
+
+    {
+      code: "MAG10",
+      installation: "Magasin 10",
+    },
+
+    {
+      code: "MAG11",
+      installation: "Cuve de gaz",
+    },
+
+    {
+      code: "MAG12",
+      installation: "Magasin 12",
+    },
+
+    {
+      code: "MAG13",
+      installation: "Soupape",
+    },
+
+    {
+      code: "MAG14",
+      installation: "Zone de trie",
+    },
+
+    {
+      code: "MAG15",
+      installation: "Chambre froide",
+    },
+
+    {
+      code: "MAG16",
+      installation: "Magasin 16",
+    },
+  ],
+
+  ZMT: [
+    {
+      code: "ZMT01",
+      installation: "Atelier maintenance",
+    },
+
+    {
+      code: "ZMT02",
+      installation: "Zone maintenance 02",
+    },
+
+    {
+      code: "ZMT03",
+      installation: "Zone maintenance 03",
+    },
+
+    {
+      code: "ZMT04",
+      installation: "Zone maintenance 04",
+    },
+
+    {
+      code: "ZMT05",
+      installation: "Zone maintenance 05",
+    },
+
+    {
+      code: "ZMT06",
+      installation: "Zone maintenance 06",
+    },
+
+    {
+      code: "ZMT07",
+      installation: "Atelier et Garage",
+    },
+
+    {
+      code: "ZMT08",
+      installation: "Zone maintenance 08",
+    },
+  ],
+
+  CON: [
+    {
+      code: "CON01",
+      installation: "Conditionnement",
+    },
+
+    {
+      code: "CON02",
+      installation: "Conditionnement 02",
+    },
+
+    {
+      code: "CON03",
+      installation: "Conditionnement 03",
+    },
+
+    {
+      code: "CON04",
+      installation: "Conditionnement 04",
+    },
+  ],
+
+  MAC: [
+    {
+      code: "MAC01",
+      installation: "Chaudière",
+    },
+
+    {
+      code: "MAC02",
+      installation: "Salle machine 02",
+    },
+
+    {
+      code: "MAC03",
+      installation: "Salle machine 03",
+    },
+
+    {
+      code: "MAC04",
+      installation: "Compresseur des fluides (air CO2)",
+    },
+
+    {
+      code: "MAC05",
+      installation: "Salle machine 05",
+    },
+
+    {
+      code: "MAC06",
+      installation: "Salle machine 06",
+    },
+
+    {
+      code: "MAC07",
+      installation: "Salle machine 07",
+    },
+
+    {
+      code: "MAC08",
+      installation: "Salle machine 08",
+    },
+
+    {
+      code: "MAC09",
+      installation: "Salle machine 09",
+    },
+
+    {
+      code: "MAC10",
+      installation: "Salle machine 10",
+    },
+
+    {
+      code: "MAC11",
+      installation: "Salle machine 11",
+    },
+  ],
+
+  TRA: [
+    {
+      code: "TRA01",
+      installation: "Matériels roulants",
+    },
+
+    {
+      code: "TRA02",
+      installation: "Transport 02",
+    },
+
+    {
+      code: "TRA03",
+      installation: "Transport 03",
+    },
+
+    {
+      code: "TRA04",
+      installation: "Matériels roulants",
+    },
+  ],
+
+  ELT: [
+    {
+      code: "ELT01",
+      installation: "Transformateur / Coffret électrique / Bloc électrique",
+    },
+
+    {
+      code: "ELT02",
+      installation: "Zone électrique 02",
+    },
+
+    {
+      code: "ELT03",
+      installation: "Transformateurs",
+    },
+
+    {
+      code: "ELT04",
+      installation: "Zone électrique 04",
+    },
+
+    {
+      code: "ELT05",
+      installation: "Groupe électrogène",
+    },
+
+    {
+      code: "ELT06",
+      installation: "Zone électrique 06",
+    },
+
+    {
+      code: "ELT07",
+      installation: "Zone électrique 07",
+    },
+  ],
+
+  CMT: [
+    {
+      code: "CMT01",
+      installation: "Quai de dépotage",
+    },
+
+    {
+      code: "CMT02",
+      installation: "Cours manutention 02",
+    },
+
+    {
+      code: "CMT03",
+      installation: "Cour",
+    },
+
+    {
+      code: "CMT04",
+      installation: "Cours manutention 04",
+    },
+  ],
+
+  CAN: [
+    {
+      code: "CAN01",
+      installation: "Canalisation de fluide",
+    },
+
+    {
+      code: "CAN02",
+      installation: "Canalisation 02",
+    },
+
+    {
+      code: "CAN03",
+      installation: "Canalisation 03",
+    },
+
+    {
+      code: "CAN04",
+      installation: "Canalisation 04",
+    },
+  ],
+
+  LAB: [
+    {
+      code: "LAB01",
+      installation: "Laboratoire",
+    },
+
+    {
+      code: "LAB02",
+      installation: "Laboratoire 02",
+    },
+
+    {
+      code: "LAB03",
+      installation: "Laboratoire 03",
+    },
+  ],
+
+  SIL: [
+    {
+      code: "SIL01",
+      installation: "Zone d’ensilage",
+    },
+
+    {
+      code: "SIL02",
+      installation: "Zone SIL 02",
+    },
+
+    {
+      code: "SIL03",
+      installation: "Zone d’ensilage du malt, maïs",
+    },
+
+    {
+      code: "SIL04",
+      installation: "Zone de désilage du maïs",
+    },
+
+    {
+      code: "SIL05",
+      installation: "Zone SIL 05",
+    },
+
+    {
+      code: "SIL06",
+      installation: "Sous-sol",
+    },
+
+    {
+      code: "SIL07",
+      installation: "Zone SIL 07",
+    },
+
+    {
+      code: "SIL08",
+      installation: "Zone SIL 08",
+    },
+
+    {
+      code: "SIL09",
+      installation: "Zone SIL 09",
+    },
+
+    {
+      code: "SIL10",
+      installation: "Zone SIL 10",
+    },
+
+    {
+      code: "SIL11",
+      installation: "Zone SIL 11",
+    },
+
+    {
+      code: "SIL12",
+      installation: "Silos",
+    },
+
+    {
+      code: "SIL13",
+      installation: "Zone SIL 13",
+    },
+
+    {
+      code: "SIL14",
+      installation: "Zone SIL 14",
+    },
+
+    {
+      code: "SIL15",
+      installation: "Zone SIL 15",
+    },
+
+    {
+      code: "SIL16",
+      installation: "Zone SIL 16",
+    },
+
+    {
+      code: "SIL17",
+      installation: "Zone SIL 17",
+    },
+
+    {
+      code: "SIL18",
+      installation: "Zone SIL 18",
+    },
+
+    {
+      code: "SIL19",
+      installation: "Station à gaz",
+    },
+  ],
+   };
 
   // =====================================
   // ADD BLOC
@@ -572,7 +911,7 @@ function APRTablePage() {
 
     <tbody>
 
-      {tableData.map((row, index) => (
+      {generatedTable.map((row, index) => (
 
         <tr key={index}>
 
